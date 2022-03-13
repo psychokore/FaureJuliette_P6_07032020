77b3a0aec5737eea09161bada206399c152c01fd
psychokore <juju.faure@yahoo.fr>
2022-03-13 15:26:52 +0100

    mentorat 13/03

--- back/app.js
+++ back/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 
 
 const userRoutes = require('./routes/user');
 
 const app = express();
-mongoose.connect('mongodb+srv://admin:K8poE4jPqR0hk5Kl@cluster0.vgsg3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(process.env.DATABASE_URI,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -18,7 +21,7 @@ mongoose.connect('mongodb+srv://admin:K8poE4jPqR0hk5Kl@cluster0.vgsg3.mongodb.ne
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
   });
 
--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, process.env.JWTOKEN);
     const userId = decodedToken.userId;
     req.auth = { userId };
     if (req.body.userId && req.body.userId !== userId) {
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: "Unauthorized"
     });
   }
 };
