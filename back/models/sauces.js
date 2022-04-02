c5182d318c247c78d87875fb7c646530c13de805
psychokore <juju.faure@yahoo.fr>
2022-04-02 11:03:48 +0200

    ajout likedislike

--- back/models/sauces.js
+++ back/models/sauces.js
@@ -8,10 +8,10 @@ const sauceSchema = mongoose.Schema({
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: {type: ["String<userId>"], required: true },
-  usersDisliked: {type: ["String<userId>"], required: true },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: {type: [String], default: [] },
+  usersDisliked: {type: [String], default: [] },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
